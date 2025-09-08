c615b469d0e61d32c5988e8827415a776e4c548f
DESKTOP-6TKUIHI\USER <syeonblue@gmail.com>
2025-09-08 21:35:33 +0900

    Revert "first commit"
    This reverts commit cdd74f63e1974dd1b06b1df4fffb25be41d80154.

--- src/store/rootSaga.js
+++
@@ -1,8 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-import roomsSaga from '../features/rooms/roomsSaga';
-import bookingsSaga from '../features/bookings/bookingsSaga';
-import userSaga from '../features/user/userSaga';
-
-export default function* rootSaga() {
-  yield all([fork(roomsSaga), fork(bookingsSaga), fork(userSaga)]);
-}
